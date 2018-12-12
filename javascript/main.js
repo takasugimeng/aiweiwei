4e3977164a58fbc312e2058c4f1b482da8f1bcef
takasugimeng <35606663+takasugimeng@users.noreply.github.com>
2018-12-12 08:50:58 -0500

    Initial commit

---
+++ javascript/main.js
@@ -0,0 +1,46 @@
+// ========================================
+// Your JavaScript goes here
+// ========================================
+
+// If you want to use the toggleClass
+// function, uncomment the function below
+// and replace '.myClass' and
+// 'class-to-toggle' with your own data.
+// Both parameters must be in quotes.
+
+// toggleClass('.myClass', 'class-to-toggle');
+
+// test1
+// function showTooltip(e) {
+//   var tooltip = e.target.classList.contains("tooltip")
+//       ? e.target
+//       : e.target.querySelector(":scope .tooltip");
+//   tooltip.style.left =
+//       (e.pageX + tooltip.clientWidth + 10 < document.body.clientWidth)
+//           ? (e.pageX + 10 + "px")
+//           : (document.body.clientWidth + 5 - tooltip.clientWidth + "px");
+//   tooltip.style.top =
+//       (e.pageY + tooltip.clientHeight + 10 < document.body.clientHeight)
+//           ? (e.pageY + 10 + "px")
+//           : (document.body.clientHeight + 5 - tooltip.clientHeight + "px");
+// }
+//
+// var tooltips = document.querySelectorAll('.couponcode');
+// for(var i = 0; i < tooltips.length; i++) {
+//   tooltips[i].addEventListener('mousemove', showTooltip);
+// }
+
+// test2
+// var tooltip = document.querySelectorAll('.coupontooltip');
+//
+// document.addEventListener('mousemove', fn, false);
+//
+// function fn(e) {
+//     for (var i=tooltip.length; i--;) {
+//         tooltip[i].style.left = e.pageX + 'px';
+//         tooltip[i].style.top = e.pageY + 'px';
+//     }
+// }
+
+
+// 反色
